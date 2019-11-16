b28b49e37c7601678ac8d2dec5a0cb2e4bedce67
cchaiyatad <chaiyatad@gmail.com>
2019-11-16 21:17:11 +0700

    gamecontroller

--- src/gamecontroller/GameController.java
+++ src/gamecontroller/GameController.java
@@ -1,14 +1,29 @@
 package gamecontroller;
 
+import gui.GameScene;
+import javafx.scene.Scene;
 import player.Player;
+import setting.Setting;
 
 public class GameController {
 //	 - Wall[] walls
 //	 - Obstacle[] obstacles
 	private Player[] players;
 
+	public Scene CreateGameScene() {
+		GameScene gameScene = new GameScene();
+		Scene scene = new Scene(gameScene, Setting.SCENE_WIDTH, Setting.SCENE_HEIGHT);
+		InputInGame inputInGame = new InputInGame(scene);
+		inputInGame.AddListeners();
+		return scene;
+	}
+	
 	public Player[] getPlayers() {
 		return players;
 	}
+	
+	
+	
+	
 	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,17 @@
 package main;
 
-import gamecontroller.InputInGame;
-import gui.GameScene;
+import gamecontroller.GameController;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import setting.Setting;
 
 public class Main extends Application{
+	private GameController gamecontroller;
 	
 	@SuppressWarnings("exports")
 	@Override
 	public void start(Stage primaryStage) {
-		GameScene gameScene = new GameScene();
-		Scene scene = new Scene(gameScene, Setting.SCENE_WIDTH, Setting.SCENE_HEIGHT);
-		InputInGame inputInGame = new InputInGame(scene);
-		inputInGame.AddListeners();
-		primaryStage.setScene(scene);
+		gamecontroller = new GameController();
+		primaryStage.setScene(gamecontroller.CreateGameScene());
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
@@ -25,4 +19,6 @@ public class Main extends Application{
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	
 }
