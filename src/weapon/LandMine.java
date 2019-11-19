57071de4a66c5ff64fbcb460bbde86067162a75f
KengAcethylene <keng2017@hotmail.com>
2019-11-19 14:48:38 +0700

    save landmine

--- src/weapon/LandMine.java
+++ src/weapon/LandMine.java
@@ -7,20 +7,8 @@ import player.Player;
 
 public class LandMine extends GameObject implements Weapon {
 
-<<<<<<< HEAD
 	public LandMine(int xPosition, int yPosition, String imagePath,Pane layer) {
 		super(xPosition, yPosition, imagePath, layer);
-||||||| fa6c769
-	public LandMine() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public LandMine(int xPosition, int yPosition, String imagePath) {
-		super(xPosition, yPosition, imagePath);
-=======
-	public LandMine(int xPosition, int yPosition, String imagePath, Pane layer) {
-		super(xPosition, yPosition, imagePath, layer);
->>>>>>> 677e5561742b3f238696b80f06f0f9a255130a7c
 		// TODO Auto-generated constructor stub
 	}
 
