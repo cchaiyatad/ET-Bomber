677583054d4a78f520ccd1fa39b1cd118638e6fd
cchaiyatad <chaiyatad@gmail.com>
2019-11-21 15:16:51 +0700

    Improve random

--- src/controller/LevelGenerator.java
+++ src/controller/LevelGenerator.java
@@ -6,9 +6,13 @@ public class LevelGenerator {
 
 	private final int obstacleRate = 85;
 	private ObjectInGame[][] objectsArray = new ObjectInGame[15][15];
+	private Random random;
+	
+	public LevelGenerator() {
+		random = new Random();
+	}
 
 	public ObjectInGame[][] generateLevel() {
-		Random random = new Random();
 		for (int i = 0; i < 15; i++) {
 			for (int j = 0; j < 15; j++) {
 				if ((i == 0 || i == 14) || (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && (j == 0 || j == 14))) {
