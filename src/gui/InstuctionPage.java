c109dc992e756ef5bfa91e0f3e5a993ba2c26652
cchaiyatad <chaiyatad@gmail.com>
2019-12-14 12:33:21 +0700

    Add legend box

--- src/gui/InstuctionPage.java
+++ src/gui/InstuctionPage.java
@@ -1,17 +1,16 @@
 package gui;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import setting.Setting;
 
 public class InstuctionPage extends VBox implements HasButtonPage {
 	private StartPage startPage;
@@ -53,11 +52,55 @@ public class InstuctionPage extends VBox implements HasButtonPage {
 		Label keyBoxHeader = new Label("KEY");
 		keyBox.getChildren().addAll(keyBoxHeader);
 		
-		GridPane ItemBox = new GridPane();
+		GridPane legendBox = createLegendBox();
 		
+		this.getChildren().addAll(headerLabel, aimOfTheGame, keyBox, legendBox);
+	}
+	private GridPane createLegendBox() {
+		GridPane legendBox = new GridPane();
+		Label legendBoxHeader = new Label("LEGEND");
 		
-		this.getChildren().addAll(headerLabel, aimOfTheGame, keyBox, ItemBox);
+		ImageView bombUpgradeImage = createImageView("bombItem");
+		Label bombUpgradeLabel = new Label("Increse number of bomb that you can place by one");
+		bombUpgradeLabel.setWrapText(true);
 		
+		ImageView powerImage = createImageView("powerItem");
+		Label powerImageLabel = new Label("Increse power of bomb by one square");
+		powerImageLabel.setWrapText(true);
 		
+		ImageView speedImage = createImageView("speedItem");
+		Label speedImageLabel = new Label("Increse running speed by one");
+		speedImageLabel.setWrapText(true);
+		
+		ImageView lifeImage = createImageView("lifeItem");
+		Label lifeImageLabel = new Label("Increse HP by one");
+		lifeImageLabel.setWrapText(true);
+		
+		ImageView shieldImage = createImageView("shield");
+		Label shieldImageLabel = new Label("Make player invulnerable to damage 5 second");
+		shieldImageLabel.setWrapText(true);
+		
+		legendBox.add(legendBoxHeader, 0, 0);
+		legendBox.add(bombUpgradeImage, 0, 1);
+		legendBox.add(bombUpgradeLabel, 1, 1);
+		legendBox.add(powerImage, 0, 2);
+		legendBox.add(powerImageLabel, 1, 2);
+		legendBox.add(speedImage, 0, 3);
+		legendBox.add(speedImageLabel, 1, 3);
+		legendBox.add(lifeImage, 0, 4);
+		legendBox.add(lifeImageLabel, 1, 4);
+		legendBox.add(shieldImage, 0, 5);
+		legendBox.add(shieldImageLabel, 1, 5);
+		
+		return legendBox;
+	}
+	
+	private ImageView createImageView(String imagePath) {
+		String path = ClassLoader.getSystemResource(String.format("image/%s.png", imagePath)).toString();
+		ImageView imageView = new ImageView(new Image(path));
+		imageView.setFitHeight(30);
+		imageView.setFitWidth(30);
+		imageView.setPreserveRatio(true);
+		return imageView;
 	}
 }
